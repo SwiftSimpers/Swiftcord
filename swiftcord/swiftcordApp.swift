e8591bf291959880f52642e4c8a2bf60cb88af56
Helloyunho <yunho050840@gmail.com>
2021-08-14 20:21:39 +0900

    Initial Commit

---
+++ swiftcord/swiftcordApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftcordApp.swift
+//  swiftcord
+//
+//  Created by Helloyunho on 2021/08/14.
+//
+
+import SwiftUI
+
+@main
+struct swiftcordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
